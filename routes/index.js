01540424c40e428c9956034aea242d738cd2563d
Pablo Cirant <cirant3002@gmail.com>
2019-06-08 10:07:30 -0400

    add eslint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express x' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
